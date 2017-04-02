a727676e2acb35afac1259b4cb380ce12c693677
FacundoBasualdo <facubasualdo49@gmail.com>
2017-04-02 15:10:16 -0300

    ejer13

---
+++ ejercicio13.java
@@ -0,0 +1,20 @@
+
+package ejercicios;
+
+
+public class ejercicio13 {
+    public static void main (String[] args){
+     
+        
+        int numero1 = 2;
+        int numero2 = 8;
+        int numero3 = 5;
+        int numero4 = 4;
+        int numero5 = 5;
+        
+        
+        float Suma = numero1 + numero2 + numero3 + numero4 + numero5;
+        float promedio = Suma / 5;
+        System.out.println(promedio);
+    }
+}
