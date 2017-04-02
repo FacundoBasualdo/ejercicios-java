cda926782ac37df9098b2dde32d4b3fc7aefa98d
FacundoBasualdo <facubasualdo49@gmail.com>
2017-04-02 14:29:37 -0300

    ejer9

---
+++ ejercicio9.java
@@ -0,0 +1,14 @@
+
+package ejercicios;
+
+public class ejercicio9 {
+    public static void main(String[] args) {
+        
+        
+        int contador = 50;
+        while (contador <= 60){
+            System.out.println(contador);
+            contador += 1;
+        }
+    }
+}
