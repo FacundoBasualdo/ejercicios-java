34f6e7e4b4e1c46dd9c742987a9cafc4bf0b1187
FacundoBasualdo <facubasualdo49@gmail.com>
2017-04-02 15:30:52 -0300

    ejer18

---
+++ ejercicio18.java
@@ -0,0 +1,17 @@
+
+package ejercicios;
+
+public class ejercicio18 {
+    public static void main(String[] args){
+        
+        
+        System.out.println("T" + " C " + "A " + "R" + " M " + "A " + "C " + "T");
+        System.out.println("P " + "P " + "P " + "P " + "P " + "P " + "P " + "P");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("P" + " P " + "P " + "P " + "P " + "P " + "P " + "P");
+        System.out.println("T " + "C " + "A " + "R " + "M " + "A " + "C " + "T");
+    }
+}
