608ccab4687c82f15fa097ead6a02a038d628dd6
FacundoBasualdo <facubasualdo49@gmail.com>
2017-04-02 14:30:53 -0300

    ejer10

---
+++ ejercicio10.java
@@ -0,0 +1,13 @@
+
+package ejercicios;
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+    
+        int contador = 0;
+        while (contador <= 40){
+            System.out.println(contador);
+            contador += 2;
+        }
+    }
+}
