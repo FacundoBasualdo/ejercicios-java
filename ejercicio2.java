9be22b30f5183afe7de00acf1a65352d5de3685f
FacundoBasualdo <facubasualdo49@gmail.com>
2017-04-02 13:55:02 -0300

    ejer2

---
+++ ejercicio2.java
@@ -0,0 +1,19 @@
+
+package ejercicios;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        
+        int CantidadBancos = 30;
+        int CantidadAlumnos = 40;
+        
+        if (CantidadBancos > CantidadAlumnos){
+            int BancosSobrantes = CantidadBancos - CantidadAlumnos;  
+            System.out.println("Sobran " + BancosSobrantes + " Bancos");
+        }
+        if (CantidadAlumnos > CantidadBancos){
+            int BancosFaltantes = CantidadAlumnos - CantidadBancos;
+            System.out.println("Faltan " + BancosFaltantes + " Bancos");
+        }
+    } 
+}
